9b28aa38a19eec2a13a79f2583a9c8d0f2c3efde
Mike Lovell <mike@Carlys-MacBook-Air.local>
2021-12-16 12:14:01 -0800

    task 3 complete

--- index.js
+++ index.js
@@ -108,9 +108,16 @@ drive(dist){
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor({name, age, location}){
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
   
 }
-
 /*
   TASK 4
     - Write an Instructor class extending Lambdasian.
